288b0e0ecc62eec86134edea24de8eb5c4131984
Marcos Aguayo <marcos@aguayo.es>
2018-07-17 12:09:22 +0200

    CORS

--- app.js
+++ app.js
@@ -25,6 +25,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/v1/', index);
 app.use('/v1/status/', index);
 app.use('/v1/mempool/', mempool);
